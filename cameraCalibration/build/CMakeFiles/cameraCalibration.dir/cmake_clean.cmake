162416b0dd419dd3cdcd2f2490c274c3854fb5c6
danielramraz <79479383+danielramraz@users.noreply.github.com>
2022-07-21 12:42:00 +0300

    Add files via upload

---
+++ cameraCalibration/build/CMakeFiles/cameraCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cameraCalibration.dir/calibration.cpp.o"
+  "CMakeFiles/cameraCalibration.dir/calibration.cpp.o.d"
+  "cameraCalibration"
+  "cameraCalibration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cameraCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cameraCalibration/build/CMakeFiles/telloCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/telloCalibration.dir/telloCalibration.cpp.o"
+  "CMakeFiles/telloCalibration.dir/telloCalibration.cpp.o.d"
+  "telloCalibration"
+  "telloCalibration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/telloCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
