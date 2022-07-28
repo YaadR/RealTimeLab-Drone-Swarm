8536588e3f95382167b08fbd843a2f875d206f03
Almog Hadad <77130590+AlmogHadad@users.noreply.github.com>
2022-07-28 12:31:25 +0300

    id output added

--- arucoReader/runAruco.cpp
+++ arucoReader/runAruco.cpp
@@ -8,7 +8,7 @@
 void runAruco(aruco &detector){
     while(true){
         std::cout << "forward: " << detector.forward << " right left: " << detector.rightLeft << " updown: " << detector.upDown
-                  << " angle: " << detector.leftOverAngle.first << " clockwise: " << detector.leftOverAngle.second << std::endl;
+                  << " angle: " << detector.leftOverAngle.first << " clockwise: " << detector.leftOverAngle.second << " id: " << detector.idr << std::endl;
     }
 }
 int arucoReaderRun(){
