3e76631c4cef80a328245e2645393d9bc110bf05
Almog Hadad <almoghadad11@gmail.com>
2022-08-04 15:03:02 +0300

    added move_drone func'

--- arucoReader/src/drone.cpp
+++ arucoReader/src/drone.cpp
@@ -7,13 +7,33 @@ drone::drone(/* args */){}
 
 drone::~drone(){}
 
-arocuLeaderInfo drone::get_leader_position(){
-    arocuLeaderInfo positionChange;
+aruco drone::get_leader_position(){
+    aruco positionChange;
     positionChange = arucoReaderRun();
     return positionChange;
 }
-void drone::move_drone(arocuLeaderInfo positionChange){
-        
+void drone::move_drone(aruco positionChange){
+    int rightDrone = positionChange.rightLeft;
+    
+    if (rightDrone >= 0) {
+        /* move to
+        Forward = 1.0;
+        rightLeft = 0.3;
+        upDown = 0;
+        angle = 0;
+        clockwise: 0 ;
+        */
+    }
+    else {
+        /* move to
+        Forward = 1.37;
+        rightLeft = -0.3;
+        upDown = 0;
+        angle = 0;
+        clockwise = 0 ;
+        */    
+    }
+
     return;
 }
 
