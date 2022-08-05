7dc4a8e4684802d0686a32c68c76326431bf36ef
Almog Hadad <77130590+AlmogHadad@users.noreply.github.com>
2022-08-05 11:32:01 +0300

    Error handling and Syntax correction

--- arucoReader/include/drone.h
+++ arucoReader/include/drone.h
@@ -2,21 +2,15 @@
 
 class drone{
     private:
-        int id;
+        // int id;
         aruco currentInfo;
         // std::vector<arocuLeaderInfo> targetsList;
 
     public:
-        drone(/* args */);
-        ~drone();
-    
-    drone::drone(/* args */){}
-    
-    drone::~drone(){}
-    
-    data get_leader_position();
-    void move_drone(data positionChange);
-    void run();
-    
-};
+        drone(aruco currentInfo);
+        // ~drone();
    
+        aruco get_leader_position();
+        void move_drone(aruco positionChange);
+        void run();
+};
