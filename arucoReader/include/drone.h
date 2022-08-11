7ba970e2b5e897c7197fb532cce36ae0c8ac8db2
danielramraz <79479383+danielramraz@users.noreply.github.com>
2022-08-11 10:33:56 +0300

    wip

--- arucoReader/include/drone.h
+++ arucoReader/include/drone.h
@@ -2,15 +2,22 @@
 
 class drone{
     private:
-        // int id;
-        aruco currentInfo;
+        int id;
+        // aruco currentInfo;
+        double upDown ;
+        double forward ;
+        double rightLeft ;
+        int idr ;
+        bool ifArucoExist ;
+        //std::pair<int, bool> leftOverAngel ;
+        bool runCamera ;
         // std::vector<arocuLeaderInfo> targetsList;
 
     public:
         drone(aruco currentInfo);
         // ~drone();
    
-        aruco get_leader_position();
-        void move_drone(aruco positionChange);
+        void get_leader_position();
+        void move_drone();
         void run();
 };
