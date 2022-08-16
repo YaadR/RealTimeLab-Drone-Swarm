8531fc0844e5706ecd5c226e45617b8f5ec5c83d
Almog Hadad <77130590+AlmogHadad@users.noreply.github.com>
2022-08-16 20:11:39 +0300

    addad "side of the drone" and more updates

--- arucoReader/runAruco.cpp
+++ arucoReader/runAruco.cpp
@@ -6,26 +6,35 @@
 #include <unistd.h>
 #include "aruco.h"
 #include "drone.h"
-#include "arocuLeaderInfo.h"
+#include <thread>
 
-
-void print_aruco_polling_output(aruco &detector, drone &myDrone){       //debug function
+void runAruco(aruco &detector, drone *myDrone){
+        while(detector.ifArucoExist != 1){
+        	std::cout << "waiting for aruco" << std::endl;
+        }
+	if(detector.rightLeft < 0)
+        	myDrone->setRightOrLeft(0); // left drone
+        else
+        	myDrone->setRightOrLeft(1); // right drone
+    	
     while(true){
-       /*std::cout 
-        << "forward: " << detector.forward 
+      /* std::cout 
+       << "forward: " << detector.forward 
         << " right left: " << detector.rightLeft 
         << " updown: " << detector.upDown
         << " angle: " << detector.leftOverAngle.first 
         << " clockwise: " << detector.leftOverAngle.second 
         << " id: " << detector.idr 
         << " read Aruco: " << detector.ifArucoExist
+        << " right or left: " << myDrone->getRightOrLeft()
         << std::endl;*/
         
-        myDrone.add_info(detector);
-        myDrone.move_drone();
+        myDrone->addInfo(detector);
+        myDrone->move_drone();
+    	//myDrone.runDrone(detector);
     }
 }
-void tzokArucoReaderRun(drone &myDrone){
+void tzukArucoReaderRun(drone *myDrone){
     std::ifstream programData("../config.json");
     nlohmann::json data;
     programData >> data;
@@ -36,16 +45,25 @@ void tzokArucoReaderRun(drone &myDrone){
     if (isCameraString){
         std::string cameraString = data["cameraString"];
         aruco detector(yamlCalibrationPath,cameraString,currentMarkerSize);
+    	runAruco(detector, myDrone);
     }else{
         int cameraPort = data["cameraPort"];
         aruco detector(yamlCalibrationPath,cameraPort,currentMarkerSize);
+    	runAruco(detector, myDrone);
     }
-    print_aruco_polling_output(detector, myDrone);    //printing func
 }
-
+/*void run(drone *myDrone){
+	myDrone->runDrone();
+}*/
 int main(){
-    drone ourDrone;
-    ourDrone.run();
-    //tzokArucoReaderRun(ourDrone);
+	system("v4l2 -d /dev/video0 focus_auto=0");
+    	drone ourDrone;
+    	tzukArucoReaderRun(&ourDrone);
+    	/*std::thread th1(tzukArucoReaderRun, &ourDrone);
+    	std::thread th2(run, &ourDrone);
+    
+    	th1.join();
+    	th2.join();*/
+    	
     return 0;
 }
